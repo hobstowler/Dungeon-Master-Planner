3b1322462b30934899684466d61e20590b89a3a7
Hobs <hobstowler@gmail.com>
2022-06-19 19:05:14 -0400

    templating skeleton

--- server.js
+++ server.js
@@ -2,19 +2,46 @@
 
 import 'dotenv/config'
 import express from "express"
+import fs from 'fs'
 
 const app = express();
 const PORT = process.env.PORT
+//const fs = require('fs') // this engine requires the fs module
 
 app.use(express.urlencoded({
     extended: true
 }))
-app.use(express.static('public'));
+
+app.engine('ntl', (filePath, options, callback) => { // define the template engine
+    fs.readFile(filePath, (err, content) => {
+        if (err) return callback(err)
+        // this is an extremely simple template engine
+        const rendered = content.toString()
+            .replace('#title#', `<title>${options.title}</title>`)
+            .replace('#navigation#', `<nav>${options.nav}</nav>`)
+            .replace('#header#', `<h1>${options.header}</h1>`)
+            .replace('#subHeader#', `<h2>${options.subHeader}</h2>`)
+            .replace('#description#', `<div>${options.description}</div>`)
+            .replace('#table#', `<table>${options.table}</table>`)
+            .replace('#input#', `<form>${options.input}</form>`)
+        return callback(null, rendered)
+    })
+})
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'ntl') // register the template engine
 
 app.get('/', (req, res) => {
-    return res.send('hello')
+    return res.render('index', {
+        title: 'new view',
+        header: 'This iss a header',
+        subHeader: 'this is the subheader',
+        nav: '',
+        description: '',
+        table: '',
+        input: ''
+    })
 })
 
 app.listen(PORT, () => {
-    console.log(`Listening on port #${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 })
