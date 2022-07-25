af9c5834356ce09541fd256eec0cecc308337d9b
Damian <damian.russ33@gmail.com>
2022-07-24 22:18:51 -0400

    updated DML

--- express/dm_planner_sample_data_manipulation_queries.sql
+++ express/dm_planner_sample_data_manipulation_queries.sql
@@ -2,8 +2,13 @@
 -- Queries for Dungeon Masters page
 -- -----------------------------------------------------
 
--- View table of all dungeon masters
-SELECT * FROM Dungeon_Masters;
+-- Retrieve metadata for Dungeon Masters table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Dungeon_Masters';
+
+-- Retrieve dungeon masters to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT dungeon_master_id AS "Dungeon Master ID", dungeon_master_name AS "Dungeon Master Name", lucky_dice AS "Lucky Dice" 
+FROM Dungeon_Masters
+WHERE dungeon_master_name LIKE '%$:nameQuery%';
 
 -- Create a new dungeon master
 INSERT INTO Dungeon_Masters (dungeon_master_name, lucky_dice)
@@ -21,18 +26,16 @@ WHERE dungeon_master_id= :dungeon_master_id_selected;
 -- Queries for Scenarios page
 -- -----------------------------------------------------
 
--- View table of all scenarios
-SELECT scenario_id, scenario_name, summary, target_level, session_time, 
-Dungeon_Masters.dungeon_master_name, Dungeons.dungeon_name
+-- Retrieve metadata for Scenarios table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Scenarios';
+
+-- Retrieve scenarios to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT scenario_id AS "Scenario ID", scenario_name AS "Scenario Name", summary AS "Summary", target_level AS "Target Level", 
+session_time AS "Session Time", Dungeon_Masters.dungeon_master_name AS "Dungeon Master", Dungeons.dungeon_name AS "Dungeon"
 FROM Scenarios
-INNER JOIN Dungeon_Masters ON Scenarios.dungeon_master_id = Dungeon_Masters.dungeon_master_id
-INNER JOIN Dungeons ON Scenarios.dungeon_id = Dungeons.dungeon_id;
-
--- Populate a dropdown for associating with a dungeon master
-SELECT dungeon_master_id, dungeon_master_name FROM Dungeon_Masters;
-
--- Populate a dropdown for associating with a dungeon
-SELECT dungeon_id, dungeon_name FROM Dungeons;
+INNER JOIN Dungeon_Masters on Dungeon_Masters.dungeon_master_id = Scenarios.dungeon_master_id
+INNER JOIN Dungeons ON Dungeons.dungeon_id = Scenarios.dungeon_id
+WHERE scenario_name LIKE '%:nameQuery%';
 
 -- Create a new scenario
 INSERT INTO Scenarios (scenario_id, scenario_name, summary, target_level, session_time, 
@@ -54,13 +57,14 @@ WHERE scenario_id= :scenario_id_selected;
 -- Queries for Dungeons page
 -- -----------------------------------------------------
 
--- View table of all dungeons
-SELECT dungeon_id, dungeon_name, description, light_level, Biomes.biome_name
-FROM Dungeons
-INNER JOIN Biomes ON Dungeons.biome_id = Biomes.biome_id;
+-- Retrieve metadata for Dungeons table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Dungeons'
 
--- Populate a dropdown for associating with a biome
-SELECT biome_id, biome_name from Biomes;
+-- Retrieve dungeons to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT dungeon_id AS "Dungeon ID", dungeon_name AS "Dungeon Name", Dungeons.description AS "Description", light_level AS "Light Level", Biomes.biome_name AS "Biome"
+FROM Dungeons
+INNER JOIN Biomes on Biomes.biome_id = Dungeons.biome_id
+WHERE dungeon_name LIKE '%:nameQuery%';
 
 -- Create a new dungeon
 INSERT INTO Dungeons (dungeon_name, description, light_level, biome_id)
@@ -79,8 +83,14 @@ WHERE dungeon_id= :dungeon_id_selected;
 -- Queries for Monsters page
 -- -----------------------------------------------------
 
--- View table of all monsters
-SELECT * FROM Monsters;
+-- Retrieve metadata for Monsters table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Monsters';
+
+-- Retrieve monsters to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT dungeon_id AS "Dungeon ID", dungeon_name AS "Dungeon Name", Dungeons.description AS "Description", light_level AS "Light Level", Biomes.biome_name AS "Biome"
+FROM Dungeons
+INNER JOIN Biomes on Biomes.biome_id = Dungeons.biome_id
+WHERE dungeon_name LIKE '%:nameQuery%';
 
 -- Create a new monster
 INSERT INTO Monsters (monster_name, description, challenge_rating, health_points, strength, 
@@ -105,12 +115,15 @@ WHERE monster_id = :monster_id_selected;
 -- Queries for Items page
 -- -----------------------------------------------------
 
--- View table of all items
-SELECT item_id, item_name, Items.description, weight, value, Types.type_name FROM Items
-INNER JOIN Types ON Items.type_id = Types.type_id;
+-- Retrieve metadata for Items table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Items';
 
--- Populate a dropdown for associating with a type
-SELECT type_id, type_name from Types;
+-- Retrieve items to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT monster_id AS "Monster ID", monster_name AS "Monster Name", description AS "Description", challenge_rating AS "Challenge Rating", 
+health_points AS "Health Points", strength AS "Strength", dexterity AS "Dexterity", constitution AS "Constitution", 
+intelligence AS "Intelligence", wisdom AS "Wisdom", charisma AS "Charisma", armor_class AS "Armor Class", talent AS "Talent(s)"
+FROM Monsters
+WHERE monster_name LIKE '%:nameQuery%';
 
 -- Create a new item
 INSERT INTO Items (item_name, description, weight, value, type_id)
@@ -129,15 +142,20 @@ WHERE item_id = :item_id_selected;
 -- Queries for Biomes page
 -- -----------------------------------------------------
 
--- View table of all biomes
-SELECT * FROM Biomes;
+-- Retrieve metadata for Biomes table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Biomes';
+
+-- Retrieve biomes to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT biome_id AS "Biome ID", biome_name AS "Biome Name", description AS "Description"
+FROM Biomes
+WHERE biome_name LIKE '%:nameQuery%';
 
 -- Create a new biome
 INSERT INTO Biomes (biome_name, description)
 VALUES (:biome_name_input, :description_input);
 
--- Delete a biome using the id of the biome selected on the Biomes page
--- NOT ALLOWED BY DATABASE
+-- Delete a biome using the id of the biome selected on the Biomes page (only allowed when no FK references exist)
+DELETE FROM Biomes WHERE biome_id = :biome_id_selected;
 
 -- Update a biome's information
 UPDATE Biomes
@@ -148,15 +166,20 @@ WHERE biome_id = :biome_id_selected;
 -- Queries for Types page
 -- -----------------------------------------------------
 
--- View table of all types
-SELECT * FROM Types;
+-- Retrieve metadata for Types table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Types';
+
+-- Retrieve types to display based on an optional search term (WHERE is only included if a value is provided for nameQuery)
+SELECT type_id AS "Type ID", type_name AS "Type Name", description AS "Description"
+FROM Types
+WHERE type_name LIKE '%:nameQuery%';
 
 -- Create a new type
 INSERT INTO Types (type_name, description)
 VALUES (:type_name_input, :description_input);
 
--- Delete a type using the id of the type selected on the Types page
--- NOT ALLOWED BY DATABASE
+-- Delete a type using the id of the type selected on the Types page (only allowed when no FK reference exists)
+DELETE FROM Types WHERE type_id= :type_id
 
 -- Update a type's information
 UPDATE Types
@@ -167,6 +190,9 @@ WHERE type_id = :type_id_selected;
 -- Queries for Dungeons_Has_Monsters page
 -- -----------------------------------------------------
 
+-- Retrieve metadata for Types table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Dungeons_Has_Monsters';
+
 -- View intersection table of all monsters associated with dungeons
 SELECT dungeon_has_monster_id, dungeon_name, monster_name, quantity
 FROM Dungeons_Has_Monsters
@@ -174,11 +200,17 @@ INNER JOIN Dungeons ON Dungeons_Has_Monsters.dungeon_id = Dungeons.dungeon_id
 INNER JOIN Monsters ON Dungeons_Has_Monsters.monster_id = Monsters.monster_id
 ORDER BY dungeon_name;
 
--- Populate a dropdown for associating with a dungeon
-SELECT dungeon_id, dungeon_name from Dungeons;
+-- View all monsters in a particular dungeon
+SELECT dungeon_has_monster_id, Monsters.monster_id, Monsters.monster_name, Monsters.description, Dungeons_Has_Monsters.quantity 
+FROM Dungeons_Has_Monsters 
+INNER JOIN Monsters ON Dungeons_Has_Monsters.monster_id = Monsters.monster_id
+WHERE Dungeons_Has_Monsters.dungeon_id = :id_of_selected_dungeon;
 
--- Populate a dropdown for associating with a monster
-SELECT monster_id, monster_name from Monsters;
+-- View all dungeons that host a particular monster
+SELECT dungeon_has_monster_id, Dungeons.dungeon_id, Dungeons.dungeon_name, Dungeons.description, Dungeons_Has_Monsters.quantity 
+FROM Dungeons_Has_Monsters 
+INNER JOIN Dungeons ON Dungeons_Has_Monsters.dungeon_id = Dungeons.dungeon_id
+WHERE Dungeons_Has_Monsters.monster_id = :id_of_selected_monster;
 
 -- Create a new relationship between Dungeons and Monsters
 INSERT INTO Dungeons_Has_Monsters (dungeon_id, monster_id, quantity)
@@ -196,6 +228,9 @@ WHERE dungeon_has_monster_id = :dungeon_has_monster_id_selected;
 -- Queries Scenarios_Has_Items page
 -- -----------------------------------------------------
 
+-- Retrieve metadata for Scenarios_Has_Items table
+SELECT * FROM `Information_Schema`.`columns` WHERE table_name='Dungeons_Has_Monsters';
+
 -- View intersection table of all items associated with scenarios
 SELECT scenario_has_item_id, scenario_name, item_name, quantity
 FROM Scenarios_Has_Items
@@ -203,11 +238,17 @@ INNER JOIN Scenarios ON Scenarios_Has_Items.scenario_id = Scenarios.scenario_id
 INNER JOIN Items ON Scenarios_Has_Items.item_id = Items.item_id
 ORDER BY scenario_name;
 
--- Populate a dropdown for associating with a scenario
-SELECT scenario_id, scenario_name from Scenarios;
+-- View all items in a particular scenario
+SELECT scenario_has_item_id, Items.item_id, item_name, Items.description, quantity 
+FROM Scenarios_Has_Items 
+INNER JOIN Items ON Scenarios_Has_Items.item_id = Items.item_id
+WHERE Scenarios_Has_Items.scenario_id = :id_of_selected_scenario;
 
--- Populate a dropdown for associating with an item
-SELECT item_id, item_name from Items;
+-- View all scenarios that host a particular item
+SELECT scenario_has_item_id, Scenarios.scenario_id, scenario_name, Scenarios.summary, quantity 
+FROM Scenarios_Has_Items
+INNER JOIN Scenarios ON Scenarios_Has_Items.scenario_id = Scenarios.scenario_id
+WHERE Scenarios_Has_Items.item_id = :id_of_selected_item;
 
 -- Create a new relationship between Scenarios and Items
 INSERT INTO Scenarios_Has_Items (scenario_id, item_id, quantity)
@@ -220,3 +261,14 @@ DELETE FROM Scenarios_Has_Items WHERE scenario_has_item_id = :scenario_has_item_
 UPDATE Scenarios_Has_Items
 SET scenario_id = :scenario_id_from_dropdown, item_id = :item_id_from_dropdown, quantity = :quantity_input
 WHERE scenario_has_item_id = :scenario_has_item_id_selected;
+
+
+-- -----------------------------------------------------
+-- Populate Dropdown Menus
+-- -----------------------------------------------------
+
+-- Retrieves metadata on foreign key usage for the table_name and column_name being referenced
+SELECT * FROM Information_Schema.key_column_usage WHERE table_name= :table_with_fk and column_name= :column_with_fk;
+
+-- Retrieves [fk_table]_id and [fk_table]_name from selected Table
+SELECT :[fk_table]_id, :[fk_table]_name FROM :fk_table;
