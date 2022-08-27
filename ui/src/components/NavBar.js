6c2335d2e4d097e70db37e0358e9ac7d49392cbb
Hobs <hobstowler@gmail.com>
2022-08-27 10:53:21 -0400

    navbar updates. new favicon

--- ui/src/components/NavBar.js
+++ ui/src/components/NavBar.js
@@ -9,6 +9,7 @@ export default function NavBar({clearData}) {
             <Link to="/dungeon_masters">Dungeon Masters</Link>
             <Link to="/scenarios">Scenarios</Link>
             <Link to="/dungeons">Dungeons</Link>
+            <Link to="/dungeons_has_monsters">Dungeons Has Monsters</Link>
             <Link to="/monsters">Monsters</Link>
             <Link to="/items">Items</Link>
             <Link to="/biomes">Biomes</Link>
